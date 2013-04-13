66c75d27bedc9c27ad36eb12a99209e1b5a4f5c0
Thanh Hai Mai <thanhhaipmai@gmail.com>
2013-04-13 15:48:45 -0700

    + add login page with bootstrap

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,6 @@
  * GET users listing.
  */
 
-exports.list = function(req, res){
-  res.send("respond with a resource");
+exports.login = function(req, res){
+  res.render('login', { title: 'Express' });
 };
