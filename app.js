3bf8a8f15441281fbe36ec902c4c5e731fe4fad5
Thanh Hai Mai <thanhhaipmai@gmail.com>
2013-04-13 16:14:08 -0700

    + add signup page

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/login', user.login);
+app.get('/signup', user.signup);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
